678c55a69c44d0ababa9cd88d3bb14cfebafbaca
Carla Ferrero <carlaferrero003@gmail.com>
2026-02-23 11:11:43 +0100

    js vérifier les champs requis et mot de passe

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -18,11 +18,27 @@ function validateForm() {
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPrenom);
     const emailOk = validateMail(inputEmail);
+    const passwordOk = validatePassword(inputPassword);
 
-    if(nomOk && prenomOk && emailOk){
+    if(nomOk && prenomOk && emailOk && passwordOk){
         btnValidation.disabled = false;
     } else {
         btnValidation.disabled = true;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
     }
 }
 
@@ -35,7 +51,6 @@ function validateMail(input){
         input.classList.remove("is-invalid");
         return true;
     } else {
-        //C'est pas ok
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
